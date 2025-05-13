7d01b3fd15794bf85136a085aac278896249d0cb
Shireen Anees Ahmed <shireenbanu89@gmail.com>
2025-05-12 23:02:39 -0700

    worked on bivariate analysis of data

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,9 +1,10 @@
 import plotly.express as px
 import pandas as pd
 from plotly.subplots import make_subplots
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-
-def histogram_and_box_plot(column_name: str, df: pd.DataFrame):
+def histogram_and_box_plot(column_name: str, df: pd.DataFrame) -> None:
     # histogram
     fig1 = px.histogram(df, x=column_name, nbins = 20, title = column_name)
     # boxplot
@@ -21,3 +22,19 @@ def histogram_and_box_plot(column_name: str, df: pd.DataFrame):
     fig.update_yaxes(title_text="Frequency",row=1, col=1)
     fig.update_xaxes(title_text=column_name, row =1, col=2)
     fig.show()
+
+
+def scatter_plot(df = pd.DataFrame, x_label = str, y_label = str)-> None :
+    plt.figure(figsize=(4,3))
+    sns.scatterplot(x=df[x_label], y=df[y_label], alpha = 0.5)
+    plt.title(f"{y_label} vs {x_label}")
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.show()
+
+def correlation_matrix(df : pd.DataFrame) -> None :
+    correlation_matrix = df.corr()
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f")
+    plt.title("Correlation Matrix of Numerical Features")
+    plt.show()
